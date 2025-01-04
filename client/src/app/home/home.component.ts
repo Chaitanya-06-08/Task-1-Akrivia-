53c52fbfb5bf753ceae9ac812d85af4a5a806910
Chaitanya <chaitanyadhavaleswarapu@gmail.com>
2025-01-04 15:47:04 +0530

    modify home component and its template

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpService } from '../services/http.service';
-import { LoadingComponent } from "../loading/loading.component";
+import { HttpService } from '../core/services/http.service';
+import { LoadingComponent } from '../loading/loading.component';
+import { logoutUrl } from '../../environments/environments';
+import { verifyTokenUrl } from '../../environments/environments';
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, LoadingComponent],
+  imports: [
+    MatButtonModule,
+    LoadingComponent,
+    RouterOutlet,
+    RouterModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -15,17 +30,16 @@ export class HomeComponent implements OnInit {
   http: HttpService = inject(HttpService);
   showProfileState: boolean = false;
   user: { email: string; password: string } | undefined = undefined;
-  private logoutUrl: string = 'http://localhost:3000/api/v1/logout';
   toaster: ToastrService = inject(ToastrService);
   loading: boolean = true;
   ngOnInit() {
-    const accessTokenUrl = 'http://localhost:3000/api/v1/verifyAccessToken';
-    this.http.get(accessTokenUrl, { withCredentials: true }).subscribe({
+    this.http.get(verifyTokenUrl, { withCredentials: true }).subscribe({
       next: (response: any) => {
         if (response?.status == false) {
           this.router.navigateByUrl('/login');
         } else {
           this.user = response?.user;
+          localStorage.setItem('user', JSON.stringify(this.user));
           this.loading = false;
           this.toaster.success(`Logged in successfully as ${this.user?.email}`);
         }
@@ -41,11 +55,7 @@ export class HomeComponent implements OnInit {
   }
   logout() {
     this.http
-      .post(
-        this.logoutUrl,
-        { email: this.user?.email },
-        { withCredentials: true }
-      )
+      .post(logoutUrl, { email: this.user?.email }, { withCredentials: true })
       .subscribe((response) => {
         console.log(response);
         localStorage.removeItem('user');
