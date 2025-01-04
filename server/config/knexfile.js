6396cfba7d6dc932b342b80985346a11c1ed241e
Chaitanya <chaitanyadhavaleswarapu@gmail.com>
2025-01-04 16:21:31 +0530

    modify server/config/knexfile

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -1,5 +1,8 @@
 const { knexSnakeCaseMappers } = require("objection");
 const path = require("path");
+require("dotenv").config({
+  path: "../.env",
+});
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -7,10 +10,10 @@ module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: "localhost",
-      database: "test",
-      user: "root",
-      password: "akv?_1234",
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: path.join(__dirname, "../db/migrations"),
